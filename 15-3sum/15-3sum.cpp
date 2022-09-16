5c5b072646cc0fa2b73e2b7954607340df0fdafa
Hargun Kaur <56452820+hkaur008@users.noreply.github.com>
2022-09-16 14:03:46 +0530

    Time: 236 ms (28.16%), Space: 19.9 MB (85.53%) - LeetHub

--- 15-3sum/15-3sum.cpp
+++ 15-3sum/15-3sum.cpp
@@ -1,35 +1,47 @@
 class Solution {
 public:
-    vector<vector<int>> threeSum(vector<int>& num) {
-        vector<vector<int>> res; 
-        sort(num.begin(), num.end()); 
+    vector<vector<int>> threeSum(vector<int>& arr) {
         
-        // moves for a
-        for (int i = 0; i < (int)(num.size())-2; i++) {
+        int n = arr.size();
+        
+        sort(arr.begin(), arr.end());
+        
+        vector<vector<int>> triples ; 
             
-            if (i == 0 || (i > 0 && num[i] != num[i-1])) {
+        for( int i = 0 ; i< n-2 ; i++ )
+        {
+            
+            if( i >0 && arr[i]==arr[i-1])
+                continue;
+            
+            int lo = i+1;
+            int  hi = n-1;
+            
+            while(lo <hi)
+            {
+                if(arr[i]+arr[lo]+arr[hi]==0)
+                {triples.push_back({arr[i], arr[lo],arr[hi]});
+                 
+                 while( lo<hi and arr[lo] == arr[lo+1])
+                      lo++;
+                 
+                 while( lo < hi and arr[hi] == arr[hi-1])
+                     hi--;
+                 
+                 lo++;
+                 hi--;
                 
-                int lo = i+1, hi = (int)(num.size())-1, sum = 0 - num[i];
-                
-                while (lo < hi) {
-                    if (num[lo] + num[hi] == sum) {
-                        
-                        vector<int> temp; 
-                        temp.push_back(num[i]); 
-                        temp.push_back(num[lo]); 
-                        temp.push_back(num[hi]); 
-                        res.push_back(temp);
-                        
-                        while (lo < hi && num[lo] == num[lo+1]) lo++;
-                        while (lo < hi && num[hi] == num[hi-1]) hi--;
-                        
-                        lo++; hi--;
-                    } 
-                    else if (num[lo] + num[hi] < sum) lo++;
-                    else hi--;
-               }
+                }
+                else if (arr[i]+arr[lo]+arr[hi]<0)
+                {lo++;}
+                else 
+                {  hi--; }
+                    
             }
         }
-        return res;
+        
+        
+        return triples;
+        
     }
 };
