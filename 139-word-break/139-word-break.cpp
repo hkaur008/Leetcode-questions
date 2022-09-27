b8b195db7acc772b78acbe6432638f6d373dfbeb
Hargun Kaur <56452820+hkaur008@users.noreply.github.com>
2022-09-27 14:14:01 +0530

    Time: 15 ms (60.94%), Space: 9.1 MB (72.16%) - LeetHub

---
+++ 139-word-break/139-word-break.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    int dp [301] ;
+    
+    int helper(int i , string s, int n , set <string>& st)
+    {
+        if( i == n) return 1;
+        if(dp[i]!=-1) return dp[i];
+        string str ;
+        
+        for( int j = i ; j< n ; j++)
+        {
+            str+=s[j];
+            if( st.find(str)!=st.end())
+            {
+                if(helper(j+1,s,n,st)) return dp[i]=1;
+            }
+        }
+        return dp[i]=0;
+    }
+    bool wordBreak(string s, vector<string>& wordDict) {
+        int n = s.size();
+        
+        memset(dp ,-1, sizeof(dp));
+        set <string> st ;
+        for(auto a : wordDict)
+        {
+            st.insert(a);
+        }
+    return helper(0, s, n , st);
+       
+     }
+};
