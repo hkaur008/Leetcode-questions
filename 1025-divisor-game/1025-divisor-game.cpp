13f70e7c0c191c9c496389e2172d3da8def9b8ed
Hargun Kaur <56452820+hkaur008@users.noreply.github.com>
2022-10-10 18:43:44 +0530

    Time: 0 ms (100.00%), Space: 5.9 MB (31.51%) - LeetHub

---
+++ 1025-divisor-game/1025-divisor-game.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int dp [1001];
+    int helper( int n )
+    {
+        if( n==1)
+            return 0;
+        if(dp[n] != -1)
+            return dp[n];
+        else{
+            for( int i = 1 ; i< n ; i++)
+            {
+                if( n%i==0)
+                    if (helper(n-i)==0)
+                        return dp[n]=1;
+            }
+            
+            return dp[n]= 0;
+        }
+    }
+    bool divisorGame(int n) {
+     
+        memset(dp, -1,sizeof(dp));
+      return  helper(n);
+    }
+};
